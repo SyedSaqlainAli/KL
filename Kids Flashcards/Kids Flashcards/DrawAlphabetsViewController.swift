a5c93170eda2b9e08199c9c9ce536fb9be6bbd3a
Saqlain-PC <saqlain-pc@Ameralds-Mac-mini.local>
2017-02-09 19:35:43 +0500

    Commit: Draw Alphabets is working perfect but there is a minor issue in drawing location when user start drawing

--- Kids Flashcards/Kids Flashcards/DrawAlphabetsViewController.swift
+++ Kids Flashcards/Kids Flashcards/DrawAlphabetsViewController.swift
@@ -14,6 +14,7 @@ class DrawAlphabetsViewController: UIViewController {
     
     
     @IBOutlet weak var alphabetImageView: UIImageView!
+    @IBOutlet weak var tempImageView: UIImageView!
     
     var currentImageIndex : Int?
     var speechSynthesizer = AVSpeechSynthesizer()
@@ -22,6 +23,10 @@ class DrawAlphabetsViewController: UIViewController {
     var lastPoint : CGPoint = CGPoint.zero
     var swiped : Bool = false
     var opacity: CGFloat = 1.0
+    var lineWidth : CGFloat = 12.0
+    var red : CGFloat = 0.0
+    var green : CGFloat = 0.0
+    var blue : CGFloat = 0.0
     
     let alphabetArray : Array = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
@@ -58,15 +63,50 @@ class DrawAlphabetsViewController: UIViewController {
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+         swiped = true
+        if let touch = touches.first! as? UITouch {
+            let currentPoint = touch.location(in: self.view)
+            drawLineFrom(fromPoint: lastPoint, toPoint: currentPoint)
+            
+            // 7
+            lastPoint = currentPoint
+        }
         
-        )
         
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        if !swiped {
+            // draw a single point
+            drawLineFrom(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+        
+        UIGraphicsBeginImageContext(view.frame.size)
+        tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: CGBlendMode.normal, alpha: 1.0)
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        
     }
     
+    func drawLineFrom(fromPoint : CGPoint , toPoint : CGPoint) {
+        
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        context?.move(to: fromPoint)
+        context?.addLine(to: toPoint)
+        context?.setLineCap(CGLineCap.round)
+        context?.setLineWidth(lineWidth)
+        context?.setStrokeColor(red: red/255, green: green/255, blue: blue/255, alpha: opacity)
+        context?.setBlendMode(CGBlendMode.normal)
+        context?.strokePath()
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        
+    }
     
     
     
@@ -91,17 +131,64 @@ class DrawAlphabetsViewController: UIViewController {
     }
     
     @IBAction func phonicLetter(_ sender: Any) {
-    
-    
+        
+        speechString = alphabetArray[currentImageIndex!] as String
+        speechUtterance = AVSpeechUtterance(string: speechString!)
+        speechSynthesizer.speak(speechUtterance!)
     }
     
     @IBAction func eraser(_ sender: Any) {
+        tempImageView.image = nil
         
     }
     
     @IBAction func colorSelector(_ sender: Any) {
         
-        print("button clicked \((sender as AnyObject).restorationIdentifier)")
+    let colorString = ((sender as AnyObject).restorationIdentifier!)! as String
+        
+        
+        switch colorString {
+        
+        case "redColor":
+            self.red = 255.0
+            self.green = 0.0
+            self.blue = 0.0
+            
+        case "orangeColor":
+            self.red = 255.0
+            self.green = 128.0
+            self.blue = 0.0
+            
+        case "greenColor":
+            self.red = 0.0
+            self.green = 255.0
+            self.blue = 0.0
+            
+        case "blueColor":
+            self.red = 0.0
+            self.green = 0.0
+            self.blue = 255.0
+            
+        case "blackColor":
+                self.red = 0.0
+                self.green = 0.0
+                self.blue = 0.0
+        default:
+            self.red = 0.0
+            self.green = 0.0
+            self.blue = 0.0
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
    
     
@@ -110,16 +197,27 @@ class DrawAlphabetsViewController: UIViewController {
         
         
         if currentImageIndex! < 25 {
-        
+        tempImageView.image = nil
         currentImageIndex = currentImageIndex! + 1
         alphabetImageView.image = UIImage(named: "draw_letter_img_\(currentImageIndex!).png")
             
             speackWord(index: currentImageIndex!)
+            print(currentImageIndex)
         }
+        else {
+            tempImageView.image = nil
+            currentImageIndex = 0
+            alphabetImageView.image = UIImage(named: "draw_letter_img_\(currentImageIndex!).png")
+            print(currentImageIndex)
+            speackWord(index: currentImageIndex!)
+        }
+        
+        
     }
     
     @IBAction func previousButton(_ sender: Any) {
         if currentImageIndex != 0 {
+            tempImageView.image = nil
             currentImageIndex = currentImageIndex! - 1
             alphabetImageView.image = UIImage(named: "draw_letter_img_\(currentImageIndex!).png")
             speackWord(index: currentImageIndex!)
